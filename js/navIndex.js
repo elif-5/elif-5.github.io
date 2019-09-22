01d8180a508d54b2c9f31c8d4e4a0c7d87b32823
glokill <446394243@qq.com>
2019-09-22 16:52:53 +0800

    step02
    增加了响应式，手机端可以使用了！

--- js/navIndex.js
+++ js/navIndex.js
@@ -7,12 +7,16 @@ function img() {
 
 img();
 window.onscroll = function () {
-    if (getScroll().top >= 100 + my$("asideNav").offsetHeight) {
-        my$("asideNav").className = "aside-nav fixed";
-        my$("asideNav").style.top = 100 + "px";
-    } else {
-        my$("asideNav").className = "aside-nav";
-        my$("asideNav").style.top = 200 + "px";
+    if (mianBaobtn.style.display=="block") {
+        my$("asideNav").style.top = 60 + "px";
+        return;
+        if (getScroll().top >= 100 + my$("asideNav").offsetHeight) {
+            my$("asideNav").className = "aside-nav fixed";
+            my$("asideNav").style.top = 100 + "px";
+         } else {
+             my$("asideNav").className = "aside-nav";
+             my$("asideNav").style.top = 200 + "px";
+    }
     }
 };
 // 以上是侧边栏固定
@@ -39,3 +43,23 @@ for (var i=0;i<lis.length;i++) {
     }
 }
 // 文章Tab栏的功能实现
+
+// 面包屑实现功能
+var mianBaobtn = my$("mianbaoxie");
+var asides =  my$("asideNav");
+
+mianBaobtn.onclick=function () {
+    if (asides.style.display!="block") {
+        asides.style.display="block";
+        my$("ani03").style.height=2+"px";
+        my$("ani03").style.border="none";
+        my$("ani01").className="ani01";
+        my$("ani02").className="ani02";
+    }else {
+        asides.style.display="none";
+        my$("ani03").style.height=0;
+        my$("ani03").style.borderBottom="2px solid #2694b0";
+        my$("ani01").className="";
+        my$("ani02").className="";
+    }
+};
